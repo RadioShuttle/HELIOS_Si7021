cf88315800a52270897c2a293d2edf62ce5990ca
Helmut Tschemernjak <>
2020-02-18 15:24:03 +0100

    Added SerialUSB support for the D21

--- examples/HELIOS_Si7021/HELIOS_Si7021.ino
+++ examples/HELIOS_Si7021/HELIOS_Si7021.ino
@@ -4,31 +4,40 @@
 #include "HELIOS_Si7021.h"
 
 
+#if defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_ARCH_SAMD)
+#define MYSERIAL SerialUSB
+#else
+#define MYSERIAL Serial
+#endif
+
+
 HELIOS_Si7021 si7021;
 
 
 void setup() {
-  Serial.begin(115200);
-
-  Serial.println("Welcome to the HELIOS_Si7021 example");
+  MYSERIAL.begin(115200);
+  while (!MYSERIAL)
+    ; // wait for serial port to connect. Needed for native USB port only
+    
+  MYSERIAL.println("Welcome to the HELIOS_Si7021 example");
   
   if (!si7021.hasSensor()) {
-    Serial.println("No Si7021 sensor found!");
+    MYSERIAL.println("No Si7021 sensor found!");
     while (true)
       ;
   }
   
-  Serial.println("Found " + String(si7021.getModelName()) + " Rev(" + String(si7021.getRevision()) + ")");
+  MYSERIAL.println("Found " + String(si7021.getModelName()) + " Rev(" + String(si7021.getRevision()) + ")");
   uint64_t serno = si7021.getSerialNumber();
-  Serial.print("Serial number: ");
-  Serial.print((uint32_t)(serno >> 32) ,HEX);
-  Serial.print(":");
-  Serial.println((uint32_t)(serno & 0xffffffff) ,HEX);
+  MYSERIAL.print("Serial number: ");
+  MYSERIAL.print((uint32_t)(serno >> 32) ,HEX);
+  MYSERIAL.print(":");
+  MYSERIAL.println((uint32_t)(serno & 0xffffffff) ,HEX);
 }
 
 void loop() {
-  Serial.print("Temperature: " + String(si7021.readTemperature(), 2) + "  ");
-  Serial.print("Humidity: " + String(si7021.readHumidity(), 1) + "%");
-  Serial.println("");
+  MYSERIAL.print("Temperature: " + String(si7021.readTemperature(), 2) + "  ");
+  MYSERIAL.print("Humidity: " + String(si7021.readHumidity(), 1) + "%");
+  MYSERIAL.println("");
   delay(1000);
 }
